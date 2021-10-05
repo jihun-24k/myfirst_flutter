40517b848bc1892e8a2a235127f0b4e3de04b7b9
jihun-24k <gi020477@gmail.com>
2021-10-05 22:52:57 +0900

    3장 실습

---
+++ lib/hello_dart.dart
@@ -0,0 +1,43 @@
+void main(){
+  Queue queue1 = new Queue();
+
+
+}
+
+class Queue{
+  List<int> _queue;
+  int _front;
+  int _rear;
+  int _MAX;
+
+  Queue(){
+    _MAX = 10000;
+    _front = 0;
+    _rear = 0;
+    _queue = new List();
+  }
+
+  void enqueue(int num){
+    List<int> list = _queue + [num];
+  }
+
+  int dequeue(){
+
+  }
+
+  int peek(){
+
+  }
+
+  bool isEmpty(){
+    return _front == _rear;
+  }
+
+  bool isFull(){
+    if(_rear == _MAX - 1){
+      return true;
+    }else{
+      return false;
+  }
+
+}
